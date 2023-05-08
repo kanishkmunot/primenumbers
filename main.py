cd8dc3c2191a7b8df86264a13de4b7c3a8ae2446
Kanishk <munotkanishk@gmail.com>
2023-05-08 20:23:58 +0530

    Hello

---
+++ main.py
@@ -0,0 +1,40 @@
+print("Hello World!")
+# Variables in Python
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+# print("Hello World2")
+
+# Variables in python
+a=3
+print(a)
+a=6.2
+print(a)
+b=9.4567
+print(b)
+c = False
+print(c)
+d="Kanishk"
+print(d)
+e = None
+print(e)
+
+# Arithmetic operators
+num1 = 10
+num2 = 3
+print("num1 + num2 is", num1 + num2)
+print("num1 - num2 is", num1 - num2)
+print("num1 * num2 is", num1 * num2)
+print("num1 / num2 is", num1 / num2)
+print("num1 // num2 is", num1 // num2)
+print("num1 % num2 is", num1 % num2)
+print("num1 ** num2 is", num1 **  num2)
+print(not(True))
+
+
+
---
+++ packagesdemo.py
@@ -0,0 +1,6 @@
+name = 'abcdefghijklmnop'
+
+print(name)
+print(name[0:3])
+# start from 0 to n-1
+
